e2e4b682f12b277c2fefb86fa353b64407e3d4ec
jateenkanoujiya <jateenkanoujiyaofficial@gmail.com>
2026-03-04 20:48:22 +0530

    Add files via upload

---
+++ Find the greatest of two numbers.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+if a>b:
+    print(f"{a} is greater than {b}")
+elif b>a:
+    print(f"{b} is greater than {a}")   
+else:
+    print(f"{a} and {b} are equal")
+    
